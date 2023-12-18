3a09abe926521fe030d44c43c6a6fdf318ab3560
hakan <hakan@y540.leg>
2023-12-18 00:20:56 -0500

    Add solution for day 17

---
+++ reto_17.js
@@ -0,0 +1,72 @@
+// En Rovaniemi, Finlandia 🇫🇮, los trineos 🛷 se alquilan por intervalos de tiempo. Cada intervalo se representa como un array de dos elementos, donde el primer elemento es el inicio del alquiler y el segundo es el final.
+
+// Por ejemplo, el array [2, 7] representa un alquiler que comienza en la hora 2 y termina en la hora 7. El problema es que a veces los intervalos se superponen entre sí, haciendo que sea un lío entender de qué hora a qué hora se alquiló el trineo.
+
+// Nos piden que, para simplificar la tarea de calcular el tiempo total de alquiler, escribamos una función que fusione todos los intervalos superpuestos y devolver un array de intervalos ordenados:
+
+// optimizeIntervals([
+//   [5, 8],
+//   [2, 7],
+//   [3, 4]
+// ]) // [[2, 8]]
+
+// optimizeIntervals([
+//   [1, 3],
+//   [8, 10],
+//   [2, 6]
+// ]) // [[1, 6], [8, 10]]
+
+// optimizeIntervals([
+//   [3, 4],
+//   [1, 2],
+//   [5, 6]
+// ]) // [[1, 2], [3, 4], [5, 6]]
+// Puedes asumir que el primer elemento de cada intervalo siempre es menor o igual que el segundo elemento. Pero los intervalos no están necesariamente ordenados.
+
+// Los números de horas pueden llegar hasta la cifra 9999.
+
+console.log(optimizeIntervals([
+    [5, 8],
+    [2, 7],
+    [3, 4]
+])) // [[2, 8]]
+
+console.log(optimizeIntervals([
+    [1, 3],
+    [8, 10],
+    [2, 6]
+])) // [[1, 6], [8, 10]]
+
+console.log(optimizeIntervals([
+    [3, 4],
+    [1, 2],
+    [5, 6]
+])) // [[1, 2], [3, 4], [5, 6]]
+
+console.log(optimizeIntervals([])) // []
+
+// 25 pts: 1682 ops/s, complexity: 12
+function optimizeIntervals(intervals) {
+    intervals = intervals.sort((a,b)=>a[0]-b[0])
+    let bases = []
+    for( const i of intervals){
+        let j = 0
+        let updated = false
+        while (j<=bases.length-1){
+            let b = bases[j]
+            if((b[0]<=i[0] && i[0]<=b[1])||(b[0]<=i[1] && i[1]<=b[1])){
+                bases.splice(j,1,[b[0]>i[0]?i[0]:b[0], b[1]>i[1]?b[1]:i[1]])
+                updated = true
+            }
+            if(i[0]<=b[0] && b[1]<=i[1]){
+                bases.splice(j, 1, i)
+                updated = true
+            }
+            j++
+        }
+        if(!updated){
+            bases.push(i)
+        }
+    }
+    return bases
+}
